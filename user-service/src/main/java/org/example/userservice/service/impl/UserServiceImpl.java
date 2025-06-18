cf3f0a0a5638060b1527033b014cfafed1ed7241
MalikBandara <mmalith520@gmail.com>
2025-06-18 16:52:14 +0530

    some updates in user-service

--- user-service/src/main/java/org/example/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/org/example/userservice/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.userservice.service.impl;
 import org.example.userservice.advisor.ConflictException;
+import org.example.userservice.advisor.NotFoundException;
 import org.example.userservice.dto.UserRequest;
 import org.example.userservice.dto.UserResponse;
 import org.example.userservice.entity.User;
@@ -35,19 +36,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponse getUserById(Long id) {
-        if(userRepository.existsById(id)){
-            User user = userRepository.findById(id).get();
-            return modelMapper.map(user, UserResponse.class);
-        }else {
-            throw new ConflictException("User with id " + id + " does not exist");
-        }
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User with id " + id + " does not exist"));
+        return modelMapper.map(user, UserResponse.class);
+
     }
 
     @Override
     public List<UserResponse> getAllUsers() {
 
         if(userRepository.findAll().isEmpty()){
-            throw new ConflictException("No users found");
+            throw new NotFoundException("No users found");
         }else {
             List<User> all = userRepository.findAll();
             return all.stream().map(user -> modelMapper.map(user, UserResponse.class)).toList();
@@ -57,17 +56,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUser(Long id) {
-        if(userRepository.findById(id).isEmpty() || !userRepository.existsById(id)){
-            throw new ConflictException("user not found with id " + id );
-        }else {
-            userRepository.deleteById(id);
-        }
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User not found with id " + id));
+        userRepository.deleteById(id);
+
     }
 
     @Override
     public UserResponse updateUser(Long id, UserRequest userRequest) {
         if(userRepository.findById(id).isEmpty() || !userRepository.existsById(id)){
-            throw new ConflictException("user not found with id " + id );
+            throw new NotFoundException("user not found with id " + id );
         }else {
             User user = userRepository.findById(id).get();
             user.setName(userRequest.getName());
